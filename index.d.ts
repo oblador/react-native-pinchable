b0370768ba48e25f88835760baa8816272ba5d78
Efren Mauricio <efren.mauricio@gmail.com>
2022-03-22 11:49:05 +0900

    Add typings

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+import { HostComponent } from 'react-native'
+type PinchableViewProps = {
+  /** The minimum allowed zoom scale
+   * @default 1
+   **/
+  minimumZoomScale?: number;
+  /** The maximum allowed zoom scale
+   * @default 3
+   **/
+  maximumZoomScale?: number;
+}
+
+/**
+ * Instagram like pinch to zoom for React Native
+ * @limitatons On Android it's not possible to receive touch events on the views inside the Pinchable component.
+ */
+declare const PinchableView: HostComponent<PinchableViewProps>;
+export default PinchableView;
